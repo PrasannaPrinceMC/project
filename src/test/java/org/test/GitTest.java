b6e4ddca165f0a968f03291ccc68cf75d921cdde
PrasannaPrinceMC <prasannaprincemc@gmail.com>
2022-11-09 12:17:46 +0530

    structure

---
+++ src/test/java/org/test/GitTest.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class GitTest {
+	
+	public static void main (String[] args) {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+	}
+
+}
